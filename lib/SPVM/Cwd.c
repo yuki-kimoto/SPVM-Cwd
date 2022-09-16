b8a7c17612782551fd6e859d09c88099e5923d32
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-09-16 09:55:54 +0900

    The realpath method in the Cwd calls the realpath method and the _fullpath method in the Sys::IO internally.
      [Changes]
        * The realpath method in the Cwd calls the realpath method and the _fullpath method in the Sys::IO internally.

--- lib/SPVM/Cwd.c
+++ lib/SPVM/Cwd.c
@@ -7,23 +7,3 @@
 
 const char* FILE_NAME = "SPVM/Cwd.c";
 
-int32_t SPVM__Cwd__realpath(SPVM_ENV* env, SPVM_VALUE* stack) {
-  
-  void* obj_file = stack[0].oval;
-  
-  if (!obj_file) {
-    return env->die(env, stack, "The file must be defined", FILE_NAME, __LINE__);
-  }
-
-  const char* file = env->get_chars(env, stack, obj_file);
-  
-  char* ret_realpath = realpath(file, NULL);
-  
-  void* obj_realpath = env->new_string(env, stack, ret_realpath, strlen(ret_realpath));
-  
-  free(ret_realpath);
-  
-  stack[0].oval = obj_realpath;
-
-  return 0;
-}
